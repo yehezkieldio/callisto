37e67eee78c8a4e57ad2990217ad9990a3474a36
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-10 05:54:07 +0800

    refactor: better switch ig

--- src/cli/handle-options.ts
+++ src/cli/handle-options.ts
@@ -4,11 +4,7 @@ import * as p from "@clack/prompts";
 import { red } from "colorette";
 
 export const handleOptions = async (options: (typeof OPTIONS)[keyof typeof OPTIONS]): Promise<void> => {
-    switch (options) {
-        case OPTIONS.BIOME:
-            await biomeHandler();
-            break;
-        default:
-            p.outro(red("Invalid option selected."));
-    }
+    options === OPTIONS.BIOME && (await biomeHandler());
+
+    if (!Object.values(OPTIONS).includes(options)) p.outro(red("Invalid option provided."));
 };
